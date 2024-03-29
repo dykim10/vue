c5f6ad570ba56fed703bfcd6810978a3871ab322
dykim10 <dykim10iufc@gmail.com>
2024-03-29 12:59:03 +0900

    webpack 알아가기

---
+++ webgame/3.숫자야구/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: {
+    app: './main.js',
+  },
+  //webpack의 핵심
+  module: {
+    rules:[{
+
+    }],
+  },
+  plugins: [],  
+  output: {
+    filename: '[name].js',
+    path: './dist',
+  },
+};
