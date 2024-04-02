9c33a1b96be99cfdc7b53b083db6d16eec94281b
dykim10 <dykim10iufc@gmail.com>
2024-04-02 16:03:23 +0900

    webpack 알아가기

--- webgame/3.숫자야구/webpack.config.js
+++ webgame/3.숫자야구/webpack.config.js
@@ -1,16 +1,25 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+
 module.exports = {
+  mode: 'development',
+  devtool : 'eval',
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
   entry: {
-    app: './main.js',
+    app: path.join(__dirname,'main.js'),
   },
   //webpack의 핵심
   module: {
     rules:[{
-
+      test: /\.vue$/,
+      loader: 'vue-loader',
     }],
   },
   plugins: [],  
   output: {
     filename: '[name].js',
-    path: './dist',
+    path: path.join(__dirname, 'dist'),//'./dist',
   },
 };
